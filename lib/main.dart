11dbd9abb51ca8fef8c70d78c9f1a462c5ba0d7f
iamfaijank <iamfaijank@gmail.com>
2026-01-09 17:44:57 +0530

    remove the debug tag at the right side of the window.

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove the debug banner
       title: 'Password Reset',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
